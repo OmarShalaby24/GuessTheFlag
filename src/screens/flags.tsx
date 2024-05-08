82ac97472961e3afefe044f52fc87a65aa52e46b
Omar Shalaby <47595444+OmarShalaby24@users.noreply.github.com>
2024-05-08 21:29:07 +0300

    Flags Search

--- src/screens/flags.tsx
+++ src/screens/flags.tsx
@@ -1,5 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
 import {RootStackParamList, CountryClass} from '../types';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RouteProp} from '@react-navigation/native';
@@ -19,6 +25,9 @@ type FlagsProps = {
 
 const Flags: React.FC<FlagsProps> = ({navigation, route}: FlagsProps) => {
   const [countries, setCountries] = useState<CountryClass[]>([]);
+  const [searchResult, setSearchResult] = useState<CountryClass[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchFinished, setSearchFinished] = useState(true);
   // const [countries, setCountries] = useState<CountryClass[]>(
   //   route.params.countries,
   // );
@@ -33,9 +42,23 @@ const Flags: React.FC<FlagsProps> = ({navigation, route}: FlagsProps) => {
     });
   };
 
+  const search = (text: string) => {
+    setSearchFinished(false);
+    setSearchQuery(text);
+    if (text.length > 2)
+      setSearchResult(
+        countries.filter(c =>
+          c.name.toLowerCase().includes(text.toLowerCase()),
+        ),
+      );
+    else setSearchResult(countries);
+    setSearchFinished(true);
+  };
+
   useEffect(() => {
     loadCountries().then((result: CountryClass[]) => {
       setCountries(result);
+      setSearchResult(result);
     });
   }, []);
   return (
@@ -46,12 +69,20 @@ const Flags: React.FC<FlagsProps> = ({navigation, route}: FlagsProps) => {
         <View>
           <Header>
             <HeaderBadge style={{width: 100}} title={'Flags'} />
+            <TextInput
+              style={styles.searchBar}
+              placeholder="Search..."
+              onChangeText={search}
+            />
             {/* <TextFiled style={{fontSize: 20, color: palette1.logo}}>
               Flags
             </TextFiled> */}
           </Header>
+          {!searchFinished && searchResult.length === 0 ? (
+            <ActivityIndicator size={'small'} color={palette1.logo} />
+          ) : null}
           <FlatList
-            data={countries}
+            data={searchResult}
             numColumns={3}
             style={{width: '100%'}}
             // windowSize={5}
@@ -74,6 +105,15 @@ const styles = StyleSheet.create({
   container: {
     height: '100%',
     backgroundColor: palette1.background,
+  },
+  searchBar: {
+    backgroundColor: palette1.text,
+    width: '60%',
+    fontFamily: 'FredokaOne-Regular',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    fontSize: 12,
   },
   btnContainer: {
     // borderWidth: 1,
